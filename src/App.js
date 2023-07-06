a2848c8755169303332e2ea2eb6b24fd11c67cc6
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-07-06 23:07:54 +0300

    add video görüntüleme

--- src/App.js
+++ src/App.js
@@ -9,21 +9,38 @@ import footer from "./components/footer"; */
 
 function App() {
   const [apod, setApod] = useState({});
-  /* const [tarih, setTarih] = useState();
+  const [date, setdate] = useState(new Date().toISOString().slice(0,10));
+  const [error,setError] = useState(null);
+  /*
   const [baslik, setBaslik] = useState ();
   const [copy, setCopy] = useState ();
   const [exp, setExp] = useState(); */
 
+
   // useEffect ile axios ile APOD'tan veri alıyoruz. useEffect bir defa okuyordu, hatırlayalım.
+  // axios ta get ile kökleri verebilirsin sonra da parametreleri de yazabilirsin, yeni bir obje açarak.
+  // .get ('user', {params: {ID:12345}})
+  // date: date bağladık dinamik olsun diye, useState i de default değerini günün tarihi yaptık.
+  //yseEffect te state değşince tekrar çalışsın diye gidip [date] yazdık, date tetiklendiğinde tekrar çalışacak fonksiyon
   useEffect (() => {
-    axios.get("https://api.nasa.gov/planetary/apod?api_key=CaxSuXdVT3QdQ2dD9TYfN14Ne1Og3TgUH1MHVJbe")
+    axios.get("https://api.nasa.gov/planetary/apod", {
+      params: {
+        api_key: "CaxSuXdVT3QdQ2dD9TYfN14Ne1Og3TgUH1MHVJbe",
+        date: date,
+      }
+      })
     .then(function(response){
       console.log(response);
       setApod(response.data);
   }).catch (function(error){
     console.log (error);
-  })}, [] )
+    setError(error.message);
+  })}, [date] )
 
+  const dateHandler = (e) => {
+    console.log (e.target.value)
+    setdate (e.target.value);
+  }
 
   return (
     <div className="App">
@@ -33,6 +50,11 @@ function App() {
         <span role="img" aria-label='go!'>
     {!Object.keys(apod).length && <p> ...Yükleniyor!!!</p>}
     {apod.title /* ya da Object.keys(apod).length yazabilirdim */ && <Header dataProp= {apod} />}
+        <input type="date" id="date" name="date" onChange={dateHandler} />
+        {/*inputu ekledik ki ekranda tarih bilgisi seçebilelim. dateHandler (bir event fnk) atadık */}
+    {error && <p> Network Error : {error}</p>}
+    {/*burda dedik ki error verirse bize git <p>error<p> yazdır. bunun da içini catch in altındaki
+    setError u tanımlayarak yaptık.*/}
     </span>
       </p>
     </div>
@@ -46,3 +68,5 @@ export default App;
 // https://apod.nasa.gov/apod/image/2307/CocoaBeach_BuckMoon_Seeley-201_1100.jpg
 // https://www.webtekno.com/images/editor/default/0004/04/a24e0ea25ae775d693dd370efec96b4a395a20dc.jpeg
 // https://api.nasa.gov/planetary/apod?api_key=CaxSuXdVT3QdQ2dD9TYfN14Ne1Og3TgUH1MHVJbe&date=2014-12-01 alternatif1
+// https://api.nasa.gov/planetary/apod?api_key=CaxSuXdVT3QdQ2dD9TYfN14Ne1Og3TgUH1MHVJbe&date=2020-04-19 videolu
+// https://api.nasa.gov/planetary/apod?api_key=CaxSuXdVT3QdQ2dD9TYfN14Ne1Og3TgUH1MHVJbe
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,27 @@
 import React from "react";
-
+// <img src={apod.url} alt = {apod.title} /> bunu da yalnızca url göstermek için kullanmıştım
 function Header (props){
     const {dataProp: apod} = props;
     return (
-        <div>
+    <div>
       <h1> {apod.title}</h1>
-      <span> {apod.title}</span>
-      <p> {apod.date}</p>
-      <h1> {apod.explanation}</h1>
-      <img src={apod.url} alt = {apod.title} />
-      </div>
+      <span> {apod.date}</span>
+      <p> {apod.explanation}</p>
+        {apod.media_type === "video" && (
+            <iframe 
+            width="560"
+            heigth="315"
+            src={apod.url}
+            title={apod.title}
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; syroscope; picture-in-picture" 
+            allowfullscreen></iframe>)
+        }
+
+        {apod.media_type === "video" && 
+            <img src={apod.url} alt={apod.title}/>  
+        }
+    </div>
       );
 }
 export default Header
