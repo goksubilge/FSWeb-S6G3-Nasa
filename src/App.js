6c1cf7a75bcd59515f621b88a6438eef4582ef30
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-08-29 18:58:32 +0300

    w/o css, runs basically

--- src/App.js
+++ src/App.js
@@ -1,66 +1,21 @@
 import React from "react";
 import "./App.css";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import Header from "./components/Header.js"
+import { useState } from "react";
+
+import Header from "./components/Header.js";
 /*
 import body from "./components/body";
 import footer from "./components/footer"; */
 
 function App() {
-  const [apod, setApod] = useState({});
-  const [date, setdate] = useState(new Date().toISOString().slice(0,10));
-  const [error,setError] = useState(null);
-  /*
-  const [baslik, setBaslik] = useState ();
-  const [copy, setCopy] = useState ();
-  const [exp, setExp] = useState(); */
-
-
-  // useEffect ile axios ile APOD'tan veri alıyoruz. useEffect bir defa okuyordu, hatırlayalım.
-  // axios ta get ile kökleri verebilirsin sonra da parametreleri de yazabilirsin, yeni bir obje açarak.
-  // .get ('user', {params: {ID:12345}})
-  // date: date bağladık dinamik olsun diye, useState i de default değerini günün tarihi yaptık.
-  //yseEffect te state değşince tekrar çalışsın diye gidip [date] yazdık, date tetiklendiğinde tekrar çalışacak fonksiyon
-  useEffect (() => {
-    axios.get("https://api.nasa.gov/planetary/apod", {
-      params: {
-        api_key: "CaxSuXdVT3QdQ2dD9TYfN14Ne1Og3TgUH1MHVJbe",
-        date: date,
-      }
-      })
-    .then(function(response){
-      console.log(response);
-      setApod(response.data);
-  }).catch (function(error){
-    console.log (error);
-    setError(error.message)
-  })}, [date] )
-
-  const dateHandler = (e) => {
-    console.log (e.target.value)
-    setdate (e.target.value);
-  }
+  const [date, setDate] = useState(new Date().toISOString().slice(0, 10));
 
   return (
     <div className="App">
-      <p>
-        {/*birinci süslü parantezde apod.title yoksa yani internet hızı yavaşken oluyo bu, 
-        title a ualşana kadar geçen sürede yükleniyor yazısı gösterilecek */} 
-        <span role="img" aria-label='go!'>
-    {!Object.keys(apod).length && <p> ...Yükleniyor!!!</p>}
-        <input type="date" id="date" name="date" onChange={dateHandler} />
-        {/*inputu ekledik ki ekranda tarih bilgisi seçebilelim. dateHandler (bir event fnk) atadık */}
-    {Object.keys(apod).length /* ya da apod.title yazabilirdim */ && <Header dataProp= {apod} />}
-    {error && <p> Network Error : {error}</p>}
-    {/*burda dedik ki error verirse bize git <p>error<p> yazdır. bunun da içini catch in altındaki
-    setError u tanımlayarak yaptık.*/}
-    </span>
-      </p>
+      <Header date={date} setDate={setDate} />
     </div>
   );
 }
-
 
 export default App;
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,73 @@
 import React from "react";
-// <img src={apod.url} alt = {apod.title} /> bunu da yalnızca url göstermek için kullanmıştım
-function Header (props){
-    const {dataProp: apod} = props;
-    return (
-    <div>
-      <h1> {apod.title}</h1>
-      <span> {apod.date}</span>
-      <p> {apod.explanation}</p>
-        {apod.media_type === "video" && (
-            <iframe 
-            width="560"
-            heigth="315"
-            src={apod.url}
-            title={apod.title}
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; syroscope; picture-in-picture" 
-            allowfullscreen></iframe>)
-        }
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Main from "./Main";
 
-        {apod.media_type === "video" && 
-            <img src={apod.url} alt={apod.title}/>  
-        }
-        {apod.media_type === "image" && 
-            <img src={apod.url} alt={apod.title}/>  
-        }
+/*
+const [baslik, setBaslik] = useState ();
+const [copy, setCopy] = useState ();
+const [exp, setExp] = useState(); */
+
+const Header = (props) => {
+  const { date, setDate } = props;
+  const [apod, setApod] = useState({});
+  const [error, setError] = useState(null);
+
+  const dateHandler = (e) => {
+    console.log(e.target.value);
+    setDate(e.target.value);
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod", {
+        params: {
+          api_key: "CaxSuXdVT3QdQ2dD9TYfN14Ne1Og3TgUH1MHVJbe",
+          date: date,
+        },
+      })
+      .then(function (response) {
+        console.log(response);
+        setApod(response.data);
+        setError(null);
+      })
+      .catch(function (error) {
+        console.log(error);
+        setError(error.message);
+      })
+      .finally(function () {});
+  }, [date]);
+
+  return (
+    <div>
+      <div className="discover">
+        <div className="title">
+          <h3>CHOOSE YOUR ASTRONOMY PICTURE | VIDEO OF THE DAY</h3>
+          <p>discover the cosmos everday</p>
+        </div>
+      </div>
+      <p>
+        {/*birinci süslü parantezde apod.title yoksa yani internet hızı yavaşken oluyo bu, 
+      title a ualşana kadar geçen sürede yükleniyor yazısı gösterilecek */}
+        <span role="img" aria-label="go!">
+          {Object.keys(apod).length === 0 && !error && <p> ...Yükleniyor!!!</p>}
+          <input
+            type="date"
+            id="date"
+            name="date"
+            value={date}
+            onChange={dateHandler}
+          />
+          {/*inputu ekledik ki ekranda tarih bilgisi seçebilelim. dateHandler (bir event fnk) atadık */}
+          {Object.keys(apod).length > 0 /* ya da apod.title yazabilirdim */ &&
+            !error && <Main dataProp={apod} />}
+          {error && <p> Network Error : {error}</p>}
+          {/*burda dedik ki error verirse bize git <p>error<p> yazdır. bunun da içini catch in altındaki
+  setError u tanımlayarak yaptık.*/}
+        </span>
+      </p>
     </div>
-      );
-}
-export default Header
+  );
+};
+
+export default Header;
